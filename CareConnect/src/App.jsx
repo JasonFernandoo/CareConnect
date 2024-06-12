61cea04adf85aabd29693107e4a53a9f7dcff095
JasonFernandoo <j450n.fernando@gmail.com>
2024-06-12 12:16:44 +0700

    refactor: update web

--- CareConnect/src/App.jsx
+++ CareConnect/src/App.jsx
@@ -1,65 +1,18 @@
 import React from 'react';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Container from './Container/Container';
+import Book from './Book/Book';
+import Mass from './Mass/Mass';
+import Aid from './Aid/Aid';
 
 function App() {
     return (
-        <div className="container">
-            <div className="header">
-                <div className="header-atas">
-                    <div className="atas-kiri">
-                        <div className="profile"></div>
-                        <p>Jakarta, Indonesia</p>
-                    </div>
-                    <div className="atas-kanan">
-                        <p>Username</p>
-                        <div className="profile"></div>
-                    </div>
-                </div>
-                <div className="header-bawah">
-                    <div className="search">
-                        <input type="text" placeholder="Type to Search" />
-                    </div>
-                </div>
-            </div>
-            <div className="main-project">
-                <div className="button">
-                    <div className="button-atas">
-                        <button onClick={() => window.location.href='../Book/Book.jsx'}>Book an Ambulance</button>
-                    </div>
-                    <div className="button-bawah">
-                        <div className="button-kiri">
-                            <button onClick={() => window.location.href='../Mass/Mass.jsx'}>Mass<br />Accident</button>
-                        </div>
-                        <div className="button-kanan">
-                            <button onClick={() => window.location.href='../Aid/Aid.jsx'}>First Aid<br />Method</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="emergency">
-                    <p>Emergency Calls</p>
-                    <div className="emergency-grid">
-                        <div className="grid-1">
-                            <button>RedKar</button>
-                        </div>
-                        <div className="grid-2">
-                            <button>Polri</button>
-                        </div>
-                        <div className="grid-3">
-                            <button>LISA</button>
-                        </div>
-                        <div className="grid-4">
-                            <button>SAR</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="map">
-                    <p>Map</p>
-                    <div className="map-image">
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Routes>
+            <Route path="/" element={<Container />} />
+            <Route path="/book" element={<Book />} />
+            <Route path="/mass" element={<Mass />} />
+            <Route path="/aid" element={<Aid />} />
+        </Routes>
     );
 }
 
---
+++ CareConnect/src/Container/Container.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Container.css';
+
+function Container() {
+    const navigate = useNavigate();
+
+    return (
+        <div className="container">
+            <div className="header">
+                <div className="header-atas">
+                    <div className="atas-kiri">
+                        <div className="profile"></div>
+                        <p>Jakarta, Indonesia</p>
+                    </div>
+                    <div className="atas-kanan">
+                        <p>Username</p>
+                        <div className="profile"></div>
+                    </div>
+                </div>
+                <div className="header-bawah">
+                    <div className="search">
+                        <input type="text" placeholder="Type to Search" />
+                    </div>
+                </div>
+            </div>
+            <div className="main-project">
+                <div className="button">
+                    <div className="button-atas">
+                        <button onClick={() => navigate('/book')}>Book an Ambulance</button>
+                    </div>
+                    <div className="button-bawah">
+                        <div className="button-kiri">
+                            <button onClick={() => navigate('/mass')}>Mass<br />Accident</button>
+                        </div>
+                        <div className="button-kanan">
+                            <button onClick={() => navigate('/aid')}>First Aid<br />Method</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="emergency">
+                    <p>Emergency Calls</p>
+                    <div className="emergency-grid">
+                        <div className="grid-1">
+                            <button>RedKar</button>
+                        </div>
+                        <div className="grid-2">
+                            <button>Polri</button>
+                        </div>
+                        <div className="grid-3">
+                            <button>LISA</button>
+                        </div>
+                        <div className="grid-4">
+                            <button>SAR</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="map">
+                    <p>Map</p>
+                    <div className="map-image"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Container;
