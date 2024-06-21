e3f2a3f7445d4aab0f0743813ee9122b6fd188c9
JasonFernandoo <j450n.fernando@gmail.com>
2024-06-21 14:33:13 +0700

    refactor: update web

--- CareConnect/src/Profile/Profile.jsx
+++ CareConnect/src/Profile/Profile.jsx
@@ -6,18 +6,18 @@ function Profile() {
     const navigate = useNavigate();
 
     return (
-        <div className="container-profile">
-            <div className='top-profile'>
-                <div className="back-profile">
-                    <button onClick={() => navigate('/')}>Back</button>     
+        <div class="non-scrollable-container">
+            <div className="container-profile">
+                <div className='top-profile'>
+                    <div className="back-profile">
+                        <button onClick={() => navigate('/')}>Back</button>     
+                    </div>
+                    <div className='profile-img'></div>
+                    <div className='profile-name'>
+                        <p>Username</p>
+                    </div>
                 </div>
-                <div className='profile-img'></div>
-                <div className='profile-name'>
-                    <p>Username</p>
-                </div>
-            </div>
-            <div className='bottom'></div>
-            <div class="non-scrollable-container">
+                <div className='bottom'></div>
                 <div className="fixed-div">
                     <div className='fixed-atas'>
 
