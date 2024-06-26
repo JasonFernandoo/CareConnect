dd21d34f057631eeeab4e7e5ab17ea847a6e5240
JasonFernandoo <j450n.fernando@gmail.com>
2024-06-27 00:25:41 +0900

    refactor: deploy database

--- CareConnect/backend/server.cjs
+++ CareConnect/backend/server.cjs
@@ -1,14 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Pool } = require('pg');
+const { sql } = require('@vercel/postgres');
 const cors = require('cors');
 
 const app = express();
 const port = 5000;
-
-const pool = new Pool({
-  connectionString: 'postgres://default:ySoAlI3jbD0d@ep-proud-cherry-a186wsy9.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require'
-});
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -17,13 +13,13 @@ app.post('/api/bookings', async (req, res) => {
   console.log('Received a POST request to /api/bookings');
 
   const { location, hospitalName, emergencyType, note, nurseAssistance } = req.body;
-  const query = 'INSERT INTO bookings (location, hospitalName, emergencyType, note, nurseAssistance) VALUES ($1, $2, $3, $4, $5) RETURNING *';
-  const values = [location, hospitalName, emergencyType, note, nurseAssistance];
-  console.log('SQL Query:', query);
-  console.log('Data to be inserted:', values);
 
   try {
-    const result = await pool.query(query, values);
+    const result = await sql`
+      INSERT INTO bookings (location, "hospitalName", "emergencyType", note, "nurseAssistance")
+      VALUES (${location}, ${hospitalName}, ${emergencyType}, ${note}, ${nurseAssistance})
+      RETURNING *;
+    `;
     console.log('Data inserted into database');
     res.status(200).json({ message: 'Data inserted successfully', data: result.rows[0] });
   } catch (error) {
@@ -33,9 +29,8 @@ app.post('/api/bookings', async (req, res) => {
 });
 
 app.get('/api/bookings', async (req, res) => {
-  const query = 'SELECT * FROM bookings ORDER BY id DESC LIMIT 1'; // Assuming 'id' is the primary key
   try {
-    const result = await pool.query(query);
+    const result = await sql`SELECT * FROM bookings ORDER BY id DESC LIMIT 1;`;
     res.status(200).json(result.rows[0]); // Sending only the latest entry
   } catch (error) {
     console.error('Error querying database:', error);
