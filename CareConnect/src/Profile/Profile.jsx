65e7706c5a09d2ed5d5beaae106afc452642cd94
JasonFernandoo <j450n.fernando@gmail.com>
2024-09-09 18:55:17 +0700

    refactor: update web

--- CareConnect/src/Profile/Profile.jsx
+++ CareConnect/src/Profile/Profile.jsx
@@ -9,30 +9,32 @@ function Profile() {
         navigate('/login', { replace: true });
       };
 
-    return (
-        <div className="container-profile">
-            <div className='top-profile'>
-                <div className="back-profile">
-                    <button onClick={() => navigate('/')}>← Back</button>     
-                </div>
-                <div className='profile-img'></div>
-                <div className='profile-name'>
-                    <p>Username</p>
-                </div>
+      return (
+        <div className="profile-page">
+            <div className="back-profile">
+                <button onClick={() => navigate('/')}>← Back</button>     
             </div>
-            <div className='bottom'></div>
-            <div className="fixed-div">
-                <div className='fixed-atas'>
-                    <button>My Address</button>
-                    <button>Account</button>
-                    <button className='last-button'>Riwayat</button>
+            <div className="container-profile">
+                <div className='top-profile'>
+                    <div className='profile-img'></div>
+                    <div className='profile-name'>
+                        <p>Username</p>
+                    </div>
                 </div>
-                <div className='fixed-bawah'>
-                    <button>Notifications</button>
-                    <button>Passwords</button>
-                    <button>Privacy and Security</button>
-                    <button>Language</button>
-                    <button className='last-button' onClick={handleLogout}>Logout</button>
+                <div className='bottom'></div>
+                <div className="fixed-div">
+                    <div className='fixed-atas'>
+                        <button>My Address</button>
+                        <button>Account</button>
+                        <button className='last-button'>Riwayat</button>
+                    </div>
+                    <div className='fixed-bawah'>
+                        <button>Notifications</button>
+                        <button>Passwords</button>
+                        <button>Privacy and Security</button>
+                        <button>Language</button>
+                        <button className='last-button' onClick={handleLogout}>Logout</button>
+                    </div>
                 </div>
             </div>
         </div>
